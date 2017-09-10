5e941ac81442e4931da4257aff84ed6bbec537c1
ukaytac <Bb2As3Gh5Sd+2>
2017-09-10 19:49:03 +0300

    Mobile Nav
    Signed-off-by: ukaytac <Bb2As3Gh5Sd+2>

--- src/js/corporate-nav.js
+++ src/js/corporate-nav.js
@@ -5,8 +5,14 @@ $(document).ready(function(){
         var clicks = $(this).data('clicks');
         if (clicks) {
             $(".mobile-menu__list").animate({"margin-left": '-=280'});
+            $(".bg-overlay").fadeOut(300);
+            $(".mobile-menu__icon .fa-times").addClass("hidden");
+            $(".mobile-menu__icon .fa-bars").removeClass("hidden");
         } else {
             $(".mobile-menu__list").animate({"margin-left": '+=280'});
+            $(".bg-overlay").fadeIn(300);
+            $(".mobile-menu__icon .fa-bars").addClass("hidden");
+            $(".mobile-menu__icon .fa-times").removeClass("hidden");
         }
         $(this).data("clicks", !clicks);
     });
