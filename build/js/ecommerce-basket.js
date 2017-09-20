3497d7c0f2c213040197218b5d52ad951972f86a
borga <borgaaydin@gmail.com>
2017-09-20 16:09:45 +0200

    coupon input

--- build/js/ecommerce-basket.js
+++ build/js/ecommerce-basket.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
      $(".basket__coupon > a").click(function() {
-
+         $(".basket__coupon .container__login__form__item").show();
+         $(this).text("Kullan");
      });
 });
