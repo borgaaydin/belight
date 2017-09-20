ddc9bb1dacc6a4b8ad096f5d33a338e73c2ef6d6
borga <borgaaydin@gmail.com>
2017-09-20 17:29:29 +0200

    kontrat

---
+++ src/js/ecommerce-contract.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#uyelikSozlesmesiOnay').click(function() {
+        $('#uyelikSozlesmeModal').modal('hide');
+        $('#kampanya-uyelik-sozlesmesi').prop('checked', true);
+    });
+});
