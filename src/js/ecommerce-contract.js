3667e109ad175b1e6cd2234e5a64c94067b0319e
borga <borgaaydin@gmail.com>
2017-09-21 17:19:51 +0200

    ie line-break fix

--- src/js/ecommerce-contract.js
+++ src/js/ecommerce-contract.js
@@ -1,6 +1,18 @@
+function contractClick() {
+    $('#uyelikSozlesmeModal').modal('show');
+}
+
 $(document).ready(function(){
+
+
+    $("#lol").click(function() {
+        alert("lol");
+
+    });
+
     $('#uyelikSozlesmesiOnay').click(function() {
         $('#uyelikSozlesmeModal').modal('hide');
         $('#kampanya-uyelik-sozlesmesi').prop('checked', true);
     });
+
 });
