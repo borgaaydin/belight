95a5d03b067a92c9f973b7683c3a9362dd96b5d8
borga <borgaaydin@gmail.com>
2017-11-01 09:47:11 +0300

    css fix

--- build/js/ecommerce-basket.js
+++ build/js/ecommerce-basket.js
@@ -3,4 +3,11 @@ $(document).ready(function() {
          $(".basket__coupon .container__login__form__item").show();
          $(this).text("Kullan");
      });
+    $("#checkbox-siparis-notu").change(function() {
+        if(this.checked) {
+            $(".havale__bilgileri__siparis-notu").show();
+        } else {
+            $(".havale__bilgileri__siparis-notu").hide();
+        }
+    });
 });
